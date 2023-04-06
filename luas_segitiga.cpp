f1e987de67d8cbe626dc60a6b4973c955c2ce48b
Rangga Nurta <88100735+nrtxx@users.noreply.github.com>
2023-04-07 06:42:33 +0700

    Create luas_segitiga.cpp

---
+++ luas_segitiga.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+main()
+
+{
+    float Luas,a,t;
+
+    cout<<"==============================="<<endl;
+    cout<<"Proram Menghitung Luas Segitiga"<<endl;
+    cout<<"==============================="<<endl;
+
+    cout<<"Masukan Nilai Alas   : ";
+    cin>>a;
+    cout<<"Masukan Nilai Tinggi : ";
+    cin>>t;
+
+    Luas=(a*t)/2;
+    cout<<"Luas Lingkaran       = "<<Luas;
+
+    return 0;
+}

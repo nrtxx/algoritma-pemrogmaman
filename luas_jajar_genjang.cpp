573b2b178d9b388a353336111b24aa24ecbbbb90
Rangga Nurta <88100735+nrtxx@users.noreply.github.com>
2023-04-07 06:39:11 +0700

    Create luas_jajar_genjang.cpp

---
+++ luas_jajar_genjang.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+main()
+{
+    float Luas,a,t;
+
+    cout<<"===================================="<<endl;
+    cout<<"Program Menghitung Luas Jajargenjang"<<endl;
+    cout<<"===================================="<<endl;
+
+    cout<<"Masukan Nilai a : ";
+    cin>>a;
+    cout<<"Masukan Nilai t : ";
+    cin>>t;
+
+    Luas=a*t;
+    cout<<"Luas Jajargenjang = "<<Luas;
+    return 0;
+}

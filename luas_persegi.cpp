b20aa6d667f95fccd9400ba1835fba5510ea77a9
Rangga Nurta <88100735+nrtxx@users.noreply.github.com>
2023-04-07 06:41:51 +0700

    Create luas_persegi.cpp

---
+++ luas_persegi.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+main()
+{
+    float s,Luas;
+
+    cout<<"==============================="<<endl;
+    cout<<"Program Menghitung Luas Persegi"<<endl;
+    cout<<"==============================="<<endl;
+
+    cout<<"Masukan Nilai Sisi : ";
+    cin>>s;
+
+    Luas=s*s;
+    cout<<"Luas Persegi = "<<Luas;
+    return 0;
+}
